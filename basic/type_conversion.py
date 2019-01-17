a7ef996196841ff0ed3dc3b0eba185832386c755
Sani Kamal <sanikamal223@gmail.com>
2019-01-17 20:39:10 +0530

    add program for type conversion

---
+++ basic/type_conversion.py
@@ -0,0 +1,14 @@
+    """
+    Created by Sani Kamal
+    """
+     
+    var1 = 4;
+    number2 = "4.0"
+    print (number2)
+     
+    converted = float(var1)
+    convertint = int(number2)
+     
+     
+    num3 = var1 + converted;
+    print (num3)
