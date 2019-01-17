2f3186672470e3e14b494658baad3e65edc6b825
Sani Kamal <sanikamal223@gmail.com>
2019-01-17 20:36:21 +0530

    add variable file

---
+++ basic/variable_test.py
@@ -0,0 +1,11 @@
+    """
+    Created by Sani Kamal
+    """
+    print("hello Rashmi")
+     
+    variablename = 1;
+    print (variablename)
+     
+    variablename2 = 2;
+    variable3 = variablename + variablename2
+    print (variable3)
