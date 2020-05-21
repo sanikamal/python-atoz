ece2a2f8f40bd2f39c738bd5b2b420a89eb72992
Sani Kamal <sanikamal223@gmail.com>
2020-05-21 23:58:57 +0530

    Create prime_num.py

---
+++ control-flow/prime_num.py
@@ -0,0 +1,17 @@
+# Check for Prime Numbers
+check_prime = [26, 39, 51, 53, 57, 79, 85]
+
+# iterate through the check_prime list
+for num in check_prime:
+
+# search for factors, iterating through numbers ranging from 2 to the number itself
+    for i in range(2, num):
+
+# number is not prime if modulo is 0
+        if (num % i) == 0:
+            print("{} is NOT a prime number, because {} is a factor of {}".format(num, i, num))
+            break
+
+# otherwise keep checking until we've searched all possible factors, and then declare it prime
+        if i == num -1:    
+            print("{} IS a prime number".format(num))
