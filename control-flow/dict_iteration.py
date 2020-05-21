6bce54085a8f53245888d6336effd3ad5044f254
Sani Kamal <sanikamal223@gmail.com>
2020-05-21 21:25:15 +0530

    Create dict_iteration.py

---
+++ control-flow/dict_iteration.py
@@ -0,0 +1,62 @@
+# You would like to count the number of fruits in your basket. 
+# In order to do this, you have the following dictionary and list of
+# fruits.  Use the dictionary and list to count the total number
+# of fruits, but you do not want to count the other items in your basket.
+
+result = 0
+basket_items = {'apples': 4, 'oranges': 19, 'kites': 3, 'sandwiches': 8}
+fruits = ['apples', 'oranges', 'pears', 'peaches', 'grapes', 'bananas']
+
+#Iterate through the dictionary
+for item in basket_items:
+    #if the key is in the list of fruits, add the value (number of fruits) to result
+    if item in fruits:
+        result += basket_items[item]
+
+
+
+print(result)
+
+#Example 1
+
+result = 0
+basket_items = {'pears': 5, 'grapes': 19, 'kites': 3, 'sandwiches': 8, 'bananas': 4}
+fruits = ['apples', 'oranges', 'pears', 'peaches', 'grapes', 'bananas']
+
+#Iterate through the dictionary
+for item in basket_items:
+    #if the key is in the list of fruits, add the value (number of fruits) to result
+    if item in fruits:
+        result += basket_items[item]
+
+print(result)
+
+#Example 2
+
+result = 0
+basket_items = {'peaches': 5, 'lettuce': 2, 'kites': 3, 'sandwiches': 8, 'pears': 4}
+fruits = ['apples', 'oranges', 'pears', 'peaches', 'grapes', 'bananas']
+
+#Iterate through the dictionary
+for item in basket_items:
+    #if the key is in the list of fruits, add the value (number of fruits) to result
+    if item in fruits:
+        result += basket_items[item]
+
+print(result)
+
+
+#Example 3
+
+result = 0
+basket_items = {'lettuce': 2, 'kites': 3, 'sandwiches': 8, 'pears': 4, 'bears': 10}
+fruits = ['apples', 'oranges', 'pears', 'peaches', 'grapes', 'bananas']
+
+#Iterate through the dictionary
+for item in basket_items:
+    #if the key is in the list of fruits, add the value (number of fruits) to result
+    if item in fruits:
+        result += basket_items[item]
+
+
+print(result)
