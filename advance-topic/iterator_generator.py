2d615a61fb32963bbd687ebc9323cbfb96024347
Sani Kamal <sanikamal223@gmail.com>
2020-05-22 18:06:10 +0530

    Create iterator_generator.py

---
+++ advance-topic/iterator_generator.py
@@ -0,0 +1,21 @@
+# Implement my_enumerate
+
+lessons = ["Why Python Programming", "Data Types and Operators", "Control Flow", "Functions", "Scripting"]
+
+def my_enumerate(iterable, start=0):
+    count = start
+    for element in iterable:
+        yield count, element
+        count += 1
+
+for i, lesson in my_enumerate(lessons, 1):
+    print("Lesson {}: {}".format(i, lesson))
+
+# Chunker
+def chunker(iterable, size):
+    """Yield successive chunks from iterable of length size."""
+    for i in range(0, len(iterable), size):
+        yield iterable[i:i + size]
+
+for chunk in chunker(range(25), 4):
+    print(list(chunk))
